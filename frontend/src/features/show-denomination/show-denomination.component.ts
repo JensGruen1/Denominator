59482ccb88a6edfadd0cad2e48d12723cc979b3d
Jens Grün <jens.gruen1@web.de>
2025-09-21 13:43:14 +0200

    First frontend commit

---
+++ frontend/src/features/show-denomination/show-denomination.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-denomination',
+  imports: [],
+  templateUrl: './show-denomination.component.html',
+  styleUrl: './show-denomination.component.css'
+})
+export class ShowDenominationComponent {
+
+}
